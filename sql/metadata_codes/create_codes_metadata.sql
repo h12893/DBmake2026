24fde5e74620deef56631cab6243a5648428949d
h12893 <junshiku.han@akkodis.co.jp>
2026-03-01 13:18:14 +0900

    sql/metadata_codesの作成、sqlファイル作成
    READMEのフォルダ構成に適合するように作成（PL_metadata_codesのクエリ5種も整理）

---
+++ sql/metadata_codes/create_codes_metadata.sql
@@ -0,0 +1,13 @@
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.tables
+    WHERE name = 'metadata_codes '
+)
+BEGIN
+    CREATE TABLE metadata_codes (
+    prefecture_code INT NOT NULL PRIMARY KEY,
+    prefecture_name NVARCHAR(200) NOT NULL,
+    region_code TINYINT NOT NULL,   -- 0:「全国」, 1:「都道府県」, 2:「政令市、23区」, 3:「外国」, 4:「不詳」
+    region_type NVARCHAR(500) NULL
+);
+END;
---
+++ sql/metadata_codes/truncate_codes_metadata.sql
@@ -0,0 +1,1 @@
+TRUNCATE TABLE metadata_codes;
